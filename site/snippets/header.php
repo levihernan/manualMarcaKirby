764cb3105354246b6edbad11fde1f656dfef0259
levihernan <levi.hernan@gmail.com>
2017-05-18 17:16:00 -0300

    Armado el framework
    queda subir contenido

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,7 @@
 <?= css('assets/css/bootstrap.min.css') ?>
 <?= css('assets/css/poncho.css') ?>
 <?= css('assets/css/font-awesome.css') ?>
-<?= css('assets/css/docs.css') ?>
 <?= css('assets/css/styles.css') ?>
+<?= css('assets/css/docs.css') ?>
 
 <?= js('assets/js/jquery.js') ?>
--- site/templates/seccion-templates.php
+++ site/templates/seccion-templates.php
@@ -9,7 +9,6 @@
   <?php foreach ($page->children() as $template): ?>
     <div class="categoria" id="<?= $template->slug() ?>">
 
-
       <h2>
         <?= $template->title() ?>
       </h2>
@@ -19,12 +18,10 @@
       <?php endif ?>
       <div class="row">
         <div class="col-md-offset-4 col-md-4">
-
-          <a class="btn btn-primary btn-md btn-block" >Descargar template</a>
+          <a href="zip/<?= $template->slug() ?>.zip" class="btn btn-primary btn-md btn-block" >Descargar template</a>
         </div>
         <div class="col-md-4">
-
-          <a class="btn btn-success btn-md btn-block" >Ver demo</a>
+          <a target="_blank" href="html/<?= $template->slug() ?>.html" class="btn btn-success btn-md btn-block" >Ver demo</a>
         </div>
       </div>
       <hr>
